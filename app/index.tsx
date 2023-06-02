5b68da2bddd0b08284b895cd226737a0013cb206
Zoltan Fodor <me@zoltanfodor.dev>
2023-06-02 21:37:01 +0200

    add libs + setup Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
 
-export default function Page() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-green-500">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
+      "nativewind/babel",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
     ],
