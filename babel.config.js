7bab6df8c59ad3e09a88140f058713f7d9da807a
Zoltan Fodor <me@zoltanfodor.dev>
2023-06-03 09:54:00 +0200

    update app route inside src folder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+process.env.EXPO_ROUTER_APP_ROOT = "../../src/app";
+
 module.exports = function (api) {
   api.cache(true);
   return {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
