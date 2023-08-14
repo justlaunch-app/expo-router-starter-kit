de1dbbbe9695c7c9c7e18ad1d09448dd90b3cfbe
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-08-14 11:51:12 +0200

    style: remove unused code

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,42 +1,14 @@
 import { LanguagePickerModal } from '_components/LanguagePickerModal';
 import { MaterialTopTabs } from '_layouts/material-top-tabs';
 import { useNavigation } from 'expo-router';
-import { useEffect, useLayoutEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useLangModal } from 'src/store/lang-picker-modal.store';
-import { useSharedValue, withDelay, withTiming } from 'react-native-reanimated';
-import { Keyboard } from 'react-native';
 import { LanguagePickerModalTrigger } from 'src/components/LanguagePickerModalTrigger';
 
 export default function IndexTopTabsLayout() {
   const { setOptions } = useNavigation();
   const { visible, close } = useLangModal();
-
-  const opacity = useSharedValue(1);
-
-  const translateY = useSharedValue(0);
-
-  useEffect(() => {
-    const keyboardDidShowListener = Keyboard.addListener(
-      'keyboardDidShow',
-      () => {
-        opacity.value = withDelay(25, withTiming(0.25));
-        translateY.value = withDelay(100, withTiming(300));
-      }
-    );
-    const keyboardDidHideListener = Keyboard.addListener(
-      'keyboardDidHide',
-      () => {
-        opacity.value = withDelay(25, withTiming(1));
-        translateY.value = withDelay(100, withTiming(0));
-      }
-    );
-
-    return () => {
-      keyboardDidHideListener.remove();
-      keyboardDidShowListener.remove();
-    };
-  }, []);
 
   useLayoutEffect(() => {
     setOptions({
