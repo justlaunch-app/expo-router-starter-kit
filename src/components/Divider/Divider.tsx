4d929be00e5f269bfda697ecd44f16b487556db2
Zoltan Fodor <me@zoltanfodor.dev>
2023-08-24 18:15:13 +0200

    feat: create a device config to get user device info + create a deviceInfo comp

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Divider() {
+  return (
+    <View className="border-t border-4 border-blue-700 dark:border-gray-700 my-2 rounded" />
+  );
+}
---
+++ src/config/device.ts
@@ -0,0 +1,12 @@
+import * as Device from 'expo-device';
+
+export const deviceBrand = Device.brand;
+export const deviceModel = Device.modelName;
+export const deviceOS = Device.osName;
+export const deviceOSVersion = Device.osVersion;
+export const deviceType = Device.deviceType;
+export const deviceYearClass = Device.deviceYearClass;
+export const deviceTotalMemory = Device.totalMemory;
+export const deviceSupportedCpuArchitectures = Device.supportedCpuArchitectures;
+export const deviceIsDevice = Device.isDevice;
+export const deviceName = Device.deviceName;
