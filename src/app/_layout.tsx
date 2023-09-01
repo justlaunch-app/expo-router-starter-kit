7dfebd728b1c98c81e2b2960d16c1f5a4f609f99
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-09-01 16:04:05 +0200

    chore: uncomment useProtectedRoute call

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -131,7 +131,7 @@ export default function RootLayout() {
     }
   }, [appState.isDelayOver]);
 
-  // useProtectedRoute(); // todo: uncomment this
+  useProtectedRoute();
 
   if (!loaded || !appState.isDelayOver) {
     return (
