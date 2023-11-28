b25f3e1360bb0f8864c9ccdc9bcc3072ca3deed8
Zoltan Fodor <me@zoltanfodor.dev>
2023-11-28 21:54:07 +0100

    chore: update tailwindcss config -> disable hover on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {},
   },
