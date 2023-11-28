13815c5ebf7e14b33d1e2232bfdae2a79a7bdebe
Zoltan Fodor <me@zoltanfodor.dev>
2023-11-28 22:24:51 +0100

    chore: remove yarn -> switch to pnpm + fixing web bundle

--- metro.config.ts
+++ metro.config.ts
@@ -1,10 +1,12 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
+const config = getDefaultConfig(__dirname , {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
+
+// hook-form web fix: Read more about it https://github.com/expo/expo/issues/23322
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
 
 module.exports = config;
