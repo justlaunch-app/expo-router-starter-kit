6c484a20101d8fbb71cb8c68ada0f6de1bb4a42e
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-12-02 16:40:56 +0100

    feat: web specific layout

--- src/app/(root)/(tabs)/_layout.tsx
+++ src/app/(root)/(tabs)/_layout.tsx
@@ -1,32 +1,32 @@
-import { Link, Navigator, Tabs, usePathname } from 'expo-router';
-import { Platform, Pressable, View } from 'react-native';
+import { Link, Tabs } from 'expo-router';
+import { Platform, Pressable } from 'react-native';
 import { useColorScheme } from 'nativewind';
 import { TabBarIcon } from '_components/Icon/TabBarIcon';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Slot = Navigator.Slot;
-import { StyledText as Text } from '_components/Text/StyledText';
-import { Button } from '_components/Button/Button';
-import { classNames } from '_utils/classNames';
+import { WebLayout } from '_components/Layout/WebLayout';
+import { useTranslation } from 'react-i18next';
 
 export default function TabLayout() {
   const { colorScheme } = useColorScheme();
+  const { t } = useTranslation();
   if (Platform.OS === 'web') {
     return (
       <WebLayout
         links={[
           {
             href: '/(root)/(tabs)/(index)',
-            name: 'One',
-            isActive: (path) => path.toLowerCase() === '/',
+            name: t('tabs.one'),
+            isActive: (path) =>
+              path.toLowerCase() === '/' || path.toLowerCase().includes('feed'),
           },
           {
             href: '/(root)/(tabs)/two',
-            name: 'Two',
+            name: t('tabs.two'),
             isActive: (path) => path.toLowerCase().includes('two'),
           },
           {
             href: '/(root)/(tabs)/settings',
-            name: 'Settings',
+            name: t('tabs.settings'),
             isActive: (path) => path.toLowerCase().includes('settings'),
           },
         ]}
@@ -43,7 +43,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(index)"
         options={{
-          title: 'Tab One',
+          title: t('tabs.one'),
           tabBarIcon: ({ color }: any) => (
             <TabBarIcon name="code" color={color} />
           ),
@@ -66,83 +66,17 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: t('tabs.two'),
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
+          title: t('tabs.settings'),
           tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} />,
         }}
       />
     </Tabs>
   );
-}
-
-interface Link {
-  href: string;
-  name?: string;
-  isActive: (path: string) => boolean;
-}
-
-interface WebLayoutProps {
-  links?: Link[];
-  title?: string;
-  logoutTitle?: string;
-}
-
-function WebLayout({
-  links = [],
-  title = 'EXPO STARTER KIT',
-  logoutTitle = 'Logout',
-}: Readonly<WebLayoutProps>) {
-  const pathname = usePathname();
-
-  if (Platform.OS === 'web') {
-    return (
-      <View>
-        <View className={'p-4 z-50 bg-white shadow-sm text-white'}>
-          <Text className={'text-red-500 z-50'}>{pathname}</Text>
-          <View
-            className={'container mx-auto flex-row items-center justify-center'}
-          >
-            <Text className="text-3xl dark:text-white text-black font-semibold flex-1">
-              {title}
-            </Text>
-            <View
-              className={
-                'flex items-center justify-center gap-2 flex-row flex-1'
-              }
-            >
-              {links.map(({ href, name, isActive }) => (
-                <Link
-                  key={href}
-                  href={href}
-                  className={classNames({
-                    'font-medium text-xl hover:underline': true,
-                    'text-blue-600 dark:text-blue-500 underline':
-                      isActive(pathname),
-                  })}
-                >
-                  {name ?? (isActive(pathname) ? pathToName(pathname) : '')}
-                </Link>
-              ))}
-            </View>
-            <Button title={logoutTitle} />
-          </View>
-        </View>
-        <View className={'container mx-auto py-4'}>
-          <Slot />
-        </View>
-      </View>
-    );
-  }
-}
-
-// Utility function to extract the name from the path
-function pathToName(path: string): string {
-  const parts = path.split('/');
-  return parts[parts.length - 1];
 }
---
+++ src/components/Layout/WebLayout.tsx
@@ -0,0 +1,63 @@
+import Slot = Navigator.Slot;
+import { Navigator, usePathname, Link } from 'expo-router';
+import { Platform, View } from 'react-native';
+import { StyledText as Text } from '_components/Text/StyledText';
+import { classNames } from '_utils/classNames';
+import { pathToName } from '_utils/layout';
+import { Button } from '_components/Button/Button';
+import { WebLayoutLink } from '_types/WebLayoutLink';
+import { useTranslation } from 'react-i18next';
+import { useAuth } from '../../store/authStore/auth.store';
+
+interface WebLayoutProps {
+  links?: WebLayoutLink[];
+  title?: string;
+}
+
+export function WebLayout({
+  links = [],
+  title = 'EXPO STARTER KIT',
+}: Readonly<WebLayoutProps>) {
+  const pathname = usePathname();
+  const { t } = useTranslation();
+  const logout = useAuth(({ logout }) => logout);
+
+  if (Platform.OS === 'web') {
+    return (
+      <View>
+        <View className={'p-4 z-50 bg-white shadow-sm text-white'}>
+          <View
+            className={'container mx-auto flex-row items-center justify-center'}
+          >
+            <Text className="text-3xl dark:text-white text-black font-semibold flex-1">
+              {title}
+            </Text>
+            <View
+              className={
+                'flex items-center justify-center gap-2 flex-row flex-1'
+              }
+            >
+              {links.map(({ href, name, isActive }) => (
+                <Link
+                  key={href}
+                  href={href}
+                  className={classNames({
+                    'font-medium text-xl hover:underline': true,
+                    'text-blue-600 dark:text-blue-500 underline':
+                      isActive(pathname),
+                  })}
+                >
+                  {name ?? (isActive(pathname) ? pathToName(pathname) : '')}
+                </Link>
+              ))}
+            </View>
+            <Button title={t('auth.sign-out')} onPress={logout} />
+          </View>
+        </View>
+        <View className={''}>
+          <Slot />
+        </View>
+      </View>
+    );
+  }
+}
