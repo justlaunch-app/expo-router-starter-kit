a0e7be49a8c5067a12cb5fe518512614e5dd3e8d
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-04 23:10:07 +0100

    HOTFIX: add correct base urls

--- src/config/segment.ts
+++ src/config/segment.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@segment/analytics-react-native';
 
 export const segmentClient = createClient({
-  writeKey: process.env.EXPO_PUBLIC_SEGMENT_KEY,
+  writeKey: process.env.SEGMENT_KEY,
 });
--- src/utils/env-loader.ts
+++ src/utils/env-loader.ts
@@ -1,9 +1,9 @@
 import { TypeOf, z } from 'zod';
 
 const schema = z.object({
-  EXPO_PUBLIC_SEGMENT_KEY: z.string().url().optional(),
   EXPO_PUBLIC_API_URL: z.string().url(),
   EXPO_PUBLIC_REQRES_API_URL: z.string().url(),
+  SEGMENT_KEY: z.string().url().optional(),
 });
 
 const parsed = schema.safeParse(process.env);
@@ -13,7 +13,7 @@ if (!parsed.success) {
     '❌ Invalid environment variables:',
     JSON.stringify(parsed.error.format(), null, 4)
   );
-  process.exit(1);
+  process.exit?.(1);
 }
 
 declare global {
