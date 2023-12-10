98b7212f33db547b8e1236757b4e6ae5e212b778
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-12-10 11:21:15 +0100

    fix: :ambulance: fix `FlashList` in the `feed`, avoid displaying empty space, set explicitly height

--- src/app/(root)/(tabs)/(index)/feed.tsx
+++ src/app/(root)/(tabs)/(index)/feed.tsx
@@ -1,5 +1,5 @@
 import { FlashList } from '@shopify/flash-list';
-import { View } from 'react-native';
+import { View, } from 'react-native';
 import { Link } from 'expo-router';
 import analytics from '_utils/analytics/segment';
 
@@ -8,6 +8,7 @@ import feedData from '_assets/data/feed.json';
 
 //COMPONENTS
 import FeedItem from '_components/Feed/FeedItem';
+import { spacing } from '_utils/viewport';
 
 interface Article {
   id: number;
@@ -22,36 +23,43 @@ interface RenderItemProps {
   item: Article;
 }
 
+
 export default function Feed() {
   analytics.trackScreen('Feed');
   return (
-    <FlashList
-      data={feedData}
-      renderItem={({ item }: RenderItemProps) => (
-        <Link
-          onPress={() => {
-            analytics.trackEvent('Feed Item Pressed', {
-              feedId: item.id,
-            });
-          }}
-          href={{
-            pathname: '/feed/[id]',
-            params: {
-              id: item.id,
-              title: item.title,
-              author: item.author,
-              datePublished: item.datePublished,
-              content: item.content,
-              imgSrc: item.imgSrc,
-            },
-          }}
-        >
-          <View className="my-4">
-            <FeedItem item={item} />
+    <View className="flex-1">
+      <FlashList
+        data={feedData}
+        estimatedItemSize={150}
+        renderItem={({ item }: RenderItemProps) => (
+          <View className='w-full'>
+            <View className='h-[150px] w-full'>
+              <Link
+                className='w-full'
+                onPress={() => {
+                  analytics.trackEvent('Feed Item Pressed', {
+                    feedId: item.id,
+                  });
+                }}
+                href={{
+                  pathname: '/feed/[id]',
+                  params: {
+                    id: item.id,
+                    title: item.title,
+                    author: item.author,
+                    datePublished: item.datePublished,
+                    content: item.content,
+                    imgSrc: item.imgSrc,
+                  },
+                }}
+              >
+                <FeedItem item={item} />
+              </Link>
+            </View>
+            <View className='bg-white' style={{ height: spacing }} />
           </View>
-        </Link>
-      )}
-      estimatedItemSize={200}
-    />
+        )}
+      />
+    </View>
   );
 }
