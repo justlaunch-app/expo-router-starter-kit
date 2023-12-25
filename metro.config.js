33ded095de069d323e600ea76924cad957cedb4d
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-25 13:16:01 +0100

    HOTFIX: redo metro config

---
+++ metro.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts },
+  } = config;
+
+  return {
+    ...config,
+    resolver: {
+      ...config.resolver,
+      sourceExts: [...sourceExts, 'mjs'],
+    },
+  };
+})();
