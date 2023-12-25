8778549c4432f5ee7d0098c55dd0c30b14969446
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-25 13:21:02 +0100

    FIX: disable ESLint error for expo/metro-config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 // Learn more https://docs.expo.io/guides/customizing-metro
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
