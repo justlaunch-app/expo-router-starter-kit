c633c735e849fab8963a3d4f1fddeefcc3a756f4
Zoltan Fodor <me@zoltanfodor.dev>
2024-07-15 17:49:17 +0200

    FIX: add description / remove keyboard rn-keyboard-provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,10 +8,23 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // Providers
 import { ThemeProvider } from '@react-navigation/native';
-import { KeyboardProvider } from 'react-native-keyboard-controller'; //https://www.npmjs.com/package/react-native-keyboard-controller
+
+/**
+ * https://www.npmjs.com/package/react-native-keyboard-controller
+ * import { KeyboardProvider } from 'react-native-keyboard-controller';
+ *
+ *  On Development build you can wrap the Stack into a KeyboardProvider to handle the keyboard avoiding the  screen to be pushed up by the keyboard.
+ *
+ *  NOTE: On Expo Go the KeyboardProvider is not working since you need to link it to native.
+ *
+ *  Install: pnpm add react-native-keyboard-controller
+ *
+ *  Wrap the Stack with the KeyboardProvider -
+ * <KeyboardProvider statusBarTranslucent navigationBarTranslucent></KeyboardProvider>
+ */
 
 // Theme
-import { NAV_THEME } from 'src/theme';
+import { NAV_THEME } from '@/theme';
 import { useColorScheme } from 'nativewind';
 import '../../global.css';
 
@@ -58,27 +71,26 @@ function RootLayoutNav() {
         style={colorScheme === 'dark' ? 'light' : 'dark'}
         key={`root-status-bar-${colorScheme === 'dark' ? 'light' : 'dark'}`}
       />
-      <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
-        <ThemeProvider value={NAV_THEME[colorScheme]}>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <Stack screenOptions={{ animation: 'ios' }}>
-              <Stack.Screen name="(root)" options={{ headerShown: false }} />
-              <Stack.Screen
-                name="modal"
-                options={{
-                  title: 'Modal',
-                  presentation: 'modal',
-                  animation: 'fade_from_bottom',
 
-                  /** You have the ability to add left and right header JSX/TSX component here fx.: a Pressable Icon component or a Close Icon module*/
-                  headerRight: () => null,
-                  headerLeft: () => null,
-                }}
-              />
-            </Stack>
-          </GestureHandlerRootView>
-        </ThemeProvider>
-      </KeyboardProvider>
+      <ThemeProvider value={NAV_THEME[colorScheme]}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Stack screenOptions={{ animation: 'ios' }}>
+            <Stack.Screen name="(root)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="modal"
+              options={{
+                title: 'Modal',
+                presentation: 'modal',
+                animation: 'fade_from_bottom',
+
+                /** You have the ability to add left and right header JSX/TSX component here fx.: a Pressable Icon component or a Close Icon module*/
+                headerRight: () => null,
+                headerLeft: () => null,
+              }}
+            />
+          </Stack>
+        </GestureHandlerRootView>
+      </ThemeProvider>
     </>
   );
 }
