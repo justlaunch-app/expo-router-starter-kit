3b9723c8e1385ed666455028af1d1a9eacbd5014
Zoltan Fodor <me@zoltanfodor.dev>
2024-07-15 14:48:56 +0200

    CHORE: create a core folder + nativewindui-text comp

--- src/app/(root)/(tabs)/(index)/index.tsx
+++ src/app/(root)/(tabs)/(index)/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '@/components/core/text';
 import { useColorScheme } from 'nativewind';
 import Icon from '@/components/LucideIcon';
 
@@ -7,17 +8,17 @@ export default function Index() {
   const { colorScheme } = useColorScheme();
 
   return (
-    <View className={colorScheme === 'light' ? 'bg-red-500' : 'bg-green-500'}>
-      <Text className="text-light-text dark:text-dark-text pt-2 text-2xl text-bold text-center">
+    <View>
+      <Text variant="largeTitle" color="primary">
         Hello World {colorScheme}
       </Text>
       <Icon name="AirVent" />
-      <Text className="text-blue-500 text-xl px-8 pt-5 pb-10">
+      <Text variant="title2" color="quarternary">
         This is an example of using Material Top Tabs with Bottom navigation in expo-router
       </Text>
 
       <View className="py-12 px-8">
-        <Text className="text-blue-500">
+        <Text color="secondary">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, eveniet ut unde, nemo
           minus nisi, ullam iure exercitationem amet quia praesentium! Minima non debitis labore,
           rem odit enim itaque qui?
